e2eefd3a84ea6e06fa847d4bdd1f8d61dbfcd19d
2304777P <2304777P@student.gla.ac.uk>
2019-11-13 14:01:32 +0000

    dataStructure

---
+++ Test/Ogre/points/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Student(models.Model):
+	StudentID = models.IntegerField(unique=True)
+	currentPoints = models.IntegerField(default= 0)
+	spentPoints = models.IntegerField(default= 0)
+	totalPoints = models.IntegerField(default= 0)
+	def __str__(self):
+		return self.StudentID
