44b76f4df4dde113049bf3585062a88fc446d079
Catriona Murphy <2312695m@student.gla.ac.uk>
2019-11-20 14:16:51 +0000

    added ogre_points url to views

--- Website/Ogre/points/views.py
+++ Website/Ogre/points/views.py
@@ -8,3 +8,7 @@ def base(request):
 
 def user_login(request):
 	return render(request, 'points/login.html',{'login_message': 'Please enter your username and password'})
+
+def ogre_points(request):
+	context_dict = {}
+	return render(request, 'points/ogre_points.html', context_dict)
