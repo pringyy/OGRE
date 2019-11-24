c728891dcdd30bb4632762e74952d9401f1c662b
2304777P <2304777P@student.gla.ac.uk>
2019-11-24 16:49:31 +0000

    Login UI changes

--- Website/Ogre/Ogre/settings.py
+++ Website/Ogre/Ogre/settings.py
@@ -16,10 +16,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = '/media/'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -123,5 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATICFILES_DIRS = [STATIC_DIR]
 STATIC_URL = '/static/'
+
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
