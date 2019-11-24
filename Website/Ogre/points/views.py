4873d1530f81b96fa9fcdaab4ffe12c1192e7f8e
2304777P <2304777P@student.gla.ac.uk>
2019-11-24 17:35:31 +0000

    make login initial page

--- Website/Ogre/points/views.py
+++ Website/Ogre/points/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 
 def base(request):
 	context_dict={}
-	return render(request, 'points/base.html', context_dict)
+	return render(request, 'points/login.html', context_dict)
 
 def user_login(request):
-	return render(request, 'points/login.html',{'login_message': 'Please enter your username and password'})
+	return render(request, 'points/base.html',{'login_message': 'Please enter your username and password'})
 
 def ogre_points(request):
 	context_dict = {}
