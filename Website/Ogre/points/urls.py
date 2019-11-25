54c2dd3974216694ec82a887b47501c243ce9c43
2304777P <2304777P@student.gla.ac.uk>
2019-11-25 12:12:00 +0000

    added information pages

--- Website/Ogre/points/urls.py
+++ Website/Ogre/points/urls.py
@@ -5,6 +5,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.base, name='base'),
+    url(r'^faq/$', views.faq, name='faq'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^contact/$', views.contact, name='contact'),
     url(r'^login/$', views.user_login, name='login'),
+    url(r'^profile/$', views.profile, name='profile'),
     url(r'^ogre_points/$', views.ogre_points, name = 'ogre_points')
 ]
