0ef86fc796e3a2bd0045178d91c20897ac37f2b7
2304777P <2304777P@student.gla.ac.uk>
2019-11-25 12:12:00 +0000

    added information pages

--- Website/Ogre/points/views.py
+++ Website/Ogre/points/views.py
@@ -12,3 +12,19 @@ def user_login(request):
 def ogre_points(request):
 	context_dict = {}
 	return render(request, 'points/ogre_points.html', context_dict)
+
+def about(request):
+    context_dict = {}
+    return render(request, 'points/about.html', context_dict)
+
+def contact(request):
+    context_dict = {}
+    return render(request, 'points/contact.html', context_dict)
+
+def faq(request):
+    context_dict = {}
+    return render(request, 'points/faq.html', context_dict)
+
+def profile(request):
+    context_dict = {}
+    return render(request, 'points/profile.html', context_dict)
