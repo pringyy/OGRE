930c73531f143a4a2b9aaaeb3a2baa45162094dd
Mingfeng YE <2325714y@student.gla.ac.uk>
2019-11-30 14:53:58 +0000

    fix some issue

--- Website/Ogre/points/models.py
+++ Website/Ogre/points/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 class StudentProfileInfo(models.Model):
 	user = models.OneToOneField(User,on_delete=models.CASCADE)
 	profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
-	StudentID = models.CharField(unique=True,null=True,max_length = 7)
+	StudentID = models.CharField(unique=True,null=True,max_length = 8)
 	currentPoints = models.IntegerField(default= 0)
 	spentPoints = models.IntegerField(default= 0)
 	totalPoints = models.IntegerField(default= 0)
