f2247329d83473a7e9895c6bf24f219217fb2d77
2304777P <2304777P@student.gla.ac.uk>
2020-02-13 11:58:12 +0000

    internal to external js

---
+++ Website/Ogre/static/js/points.js
@@ -0,0 +1,20 @@
+$('document').ready(function(){
+    
+ $('#loginhref').text('Logout');
+ $('#loginhref').attr('href', '/logout');
+ 
+          $.ajax({
+            url: '/getmypoint/',
+            type: "GET",
+            dataType:'json',
+          }).done(function( responce ) {
+              var status = responce.status;
+              
+              $('.alert').hide();
+              
+             if(status == '1'){
+                  $("#points").html(responce.points)
+            })
+           
+
+        });
