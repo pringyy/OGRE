c00b86116610c3aac83ae90d0c6691e025eab7db
Mingfeng YE <2325714y@student.gla.ac.uk>
2020-02-19 21:48:15 +0000

    moodle logic code for the change usename and cut its points for 5 --- 2

--- api/update_username.php
+++ api/update_username.php
@@ -6,3 +6,8 @@ include(dirname(dirname(__FILE__)).'/include/config.php');
 if(isset($_REQUEST['user_id'])  ){
 
 $user_id = $_REQUEST['user_id'];
+
+
+$sql = "SELECT * FROM mdl_user WHERE id = '".$user_id."' ";
+$result = mysqli_query($con, $sql);
+if(mysqli_num_rows($result) > 0){
