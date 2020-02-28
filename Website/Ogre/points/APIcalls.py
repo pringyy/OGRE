1565c92e02c5ad8476090880fc02307ca04eca14
2304777P <2304777P@student.gla.ac.uk>
2020-02-28 19:30:50 +0000

    comments in APIcalls.py

--- Website/Ogre/points/APIcalls.py
+++ Website/Ogre/points/APIcalls.py
@@ -1,3 +1,7 @@
+
+#HERE IS WHERE YOU CHANGE THE URL TO YOUR MOODLE SERVER
+#MAKE SURE ALL THE PHP FILES ARE IN THE API FOLDER FOR THIS TO WORK
+
 loginAPIcall = 'http://157.245.126.159/api/login.php'
 getPointsAPIcall = 'http://157.245.126.159/api/get_user_points.php?user_id='
 removePointsAPIcall = 'http://157.245.126.159/api/cut_user_points.php?user_id='
