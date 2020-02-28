7dc02d3c55ec400f0282f7e4cb1c4f6df6d77341
Mingfeng YE <2325714y@student.gla.ac.uk>
2020-02-28 17:17:16 +0000

    modify php api code

--- api/get_leaderboard.php
+++ api/get_leaderboard.php
@@ -13,17 +13,19 @@ if(isset($_REQUEST['user_id'])){
     if(mysqli_num_rows($result) > 0){
 
 
-    $sql = "SELECT * FROM mdl_user_points WHERE user_id = '".$user_id."' ";
+    $sql = "SELECT * FROM mdl_user_points order by points desc ";
     if($result = mysqli_query($con, $sql)){
     if(mysqli_num_rows($result) > 0){
        
-       $row = mysqli_fetch_assoc($result);
-       $points = $row['points'];
-       $user_id = $row['user_id'];
-       
-        $data = array('status'=>1,'user_id'=>$user_id, 'points'=>$points);
-        echo json_encode($data);
-        exit;
+		while($row = mysqli_fetch_assoc($result)){
+			$data[]=$row;
+		}
+		 
+		 $total=mysqli_num_rows($result);
+		 
+		 $data = array('status'=>1,"current"=> 1,"rowCount"=>10,'user_id'=>$user_id,'total'=>$total,'rows'=>$data);
+		 echo json_encode($data);
+		 exit;
         
     }else{
         $data = array('status'=>0,'message'=>'No User points found for this user.');
