98ed1e2443a5d6f754b00b59a38cb6909f063051
2304777P <2304777P@student.gla.ac.uk>
2020-03-01 21:28:00 +0000

    comment changes

--- Website/Ogre/points/models.py
+++ Website/Ogre/points/models.py
@@ -1,9 +1,11 @@
 import time, uuid
 from django.db import models
 from django.contrib.auth.models import User
+
+#Used to represent the Student database model
 class StudentProfileInfo(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
-    StudentID = models.CharField(unique=True,null=True,max_length = 8,help_text='Required. the StudentID must your moodle studentID')
+    StudentID = models.CharField(unique=True,null=True,max_length = 8,help_text='Please enter your student ID you use for Moodle.')
     def __str__(self):
         return (self.user.username)
