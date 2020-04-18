26bd1bc06e1d14129b8922da8c9bb7f21b7ec457
Robert Pringle <46602946+pringyy@users.noreply.github.com>
2020-04-18 14:42:22 +0100

    Delete secretsettings.py

--- Website/Ogre/Ogre/secretsettings.py
+++
@@ -1,5 +0,0 @@
-#THIS FILE IS USED FOR SECRET SETTINGS THAT ARE SUPPOSED TO BE HIDDEN
-
-#Password for the email in the contact page
-EMAIL = 'contactogre2020@gmail.com'
-EMAIL_PASSWORD = 'jbccahgrsoaflxbr'
